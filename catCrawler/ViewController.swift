4a1ff2867596429d60c1bae83a7ef0e6edbf04f4
luninj <103933747+luninj@users.noreply.github.com>
2022-07-16 01:17:59 +0900

    기초 레이아웃 잡기

--- catCrawler/ViewController.swift
+++ catCrawler/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private enum Matrics {
+        static let inset : CGFloat = 4
+    }
+    
+    private let collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        layout.minimumLineSpacing = Matrics.inset
+        layout.minimumInteritemSpacing = Matrics.inset
+        return cv
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setupView()
     }
-
+    
+    private func setupView() {
+        self.view.addSubview(self.collectionView)
+        
+        self.collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+            
+        ])
+        
+        self.collectionView.backgroundColor = .white
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.reloadData()
+    }
 
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView : UICollectionView, layout collectionViewLayout : UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.frame.width
+        
+        let cellWidth = (width - 2 * Matrics.inset) / 3
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+}
+
+extension ViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell" , for: indexPath)
+        cell.backgroundColor = .black
+        return cell
+    }
+    
+    
+}
